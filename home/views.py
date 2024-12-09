9776a0064fe961954a5056a3be5915dfd2ac03e4
Rosemary <rosemarysilva78@hotmail.com>
2024-12-09 15:22:38 -0300

    atividade de Pweb

---
+++ home/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Categoria
+
+
+def index(request):
+    return render(request,'index.html')
+
+def categoria(request):
+    contexto = {
+        'lista': Categoria.objects.all().order_by(id),
+
+    }
+    return render(request,'categoria/lista.html',contexto)
+         
